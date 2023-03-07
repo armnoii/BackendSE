37149c774f5d9d7c5a4db0ffde8b939c8e3f7192
armnoii <86842739+armnoii@users.noreply.github.com>
2023-03-07 21:03:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,17 +5,76 @@ const app = express();
 const QRCode = require('qrcode')
 const generatePayload = require('promptpay-qr')
 const bodyParser = require('body-parser')
-const _ = require('lodash')
-const port = 3000;
+const _ = require('lodash');
+const { response } = require('express');
+const port = 80;
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+var amount = 500000;
+var currency = 'thb';
+
+var omise = require('omise')({
+  'publicKey': process.env.OMISE_PUBLIC_KEY,
+  'secretKey': process.env.OMISE_SECRET_KEY
+});
+
+var cardDetails = {
+  card: {
+    'name': 'arm',
+    'city': 'Bangkok',
+    'postal_code': 10160,
+    'number': '4242424242424242',
+    'expiration_month': 12,
+    'expiration_year': 2024,
+  },
+};
+
+var source = {
+  'type': 'internet_banking_bbl',
+  'amount': 500000,
+  'currency': 'thb',
+};
+
+// omise.sources.create(source).then(function(resSource) {
+//   return omise.charges.create({
+//     'amount':     amount,
+//     'source':     resSource.id,
+//     'currency':   currency,
+//     'return_uri': 'http://localhost:3000/message',
+//   });
+// }).then(function(charge) {
+//   console.log(charge);
+// }).catch(function(err) {
+//   console.log(err);
+// });
+
+
+// omise.tokens.create(cardDetails, function (err, token) {
+//   omise.charges.create({
+//     amount: 120000,
+//     currency: 'thb',
+//     card: token.id,
+//     metadata: {
+//       note: 'test card'
+//     }
+//   }, function (err, charge) {
+//     if (err) {
+//       console.log('error', err);
+//       return;
+//     }
+//     console.log('charge', charge);
+//   });
+// });
 
 mongoose.connect("mongodb://admin:1234@ac-7c0mbnp-shard-00-00.u5fneuc.mongodb.net:27017,ac-7c0mbnp-shard-00-01.u5fneuc.mongodb.net:27017,ac-7c0mbnp-shard-00-02.u5fneuc.mongodb.net:27017/?ssl=true&replicaSet=atlas-r19x1c-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: "true",
@@ -27,75 +86,59 @@ mongoose.connection.on("connected", (err, res) => {
   console.log("mongoose is connected")
 })
 
-const mageSchema = new mongoose.Schema({
-  name: {
-      type: String,
-      require: true
-  },
-  power_type: {
-      type: String,
-      require: true
-  },
-  mana_power: Number,
-  health: Number,
-  gold: Number
+const customersSchema = new mongoose.Schema({
+  accounts: String,
+  active: String,
+  address: String,
+  birthdate: String,
+  email: String,
+  name: String,
+  username: String
 })
 
-const Mage = new mongoose.model("Mage", mageSchema)
+const Customers = new mongoose.model("customers", customersSchema)
 
-const createManyDocuments = async () =>{
-  try {
-   await Person.deleteMany({});    
-   let persons = await Person.create(
-    [
-       {   
-         name: "Theodore",
-         title: "MERN Stack Developer",
-         commit: 1000
-       },
-       {   
-         name: "Chidera",
-         title: "Network Administrator",
-         commit: 1000
-       },
-       {
-         name: "Elijah",
-         title: "Backend Developer",
-         commit: 200
-       },
-       {
-         name: "Emeka",
-         title: "Web Developer",
-         commit: 500
-       }
-     ]
-   )
-   return persons
- }
- catch (error) {
-   throw error
- }
-}
-
-app.get("/create", async (req, res) => {
-  let persons = await createManyDocuments();
-  res.json({documents: persons});
-});
-
-app.get("/findname", async (req, res)=>{
-  Mage.find({}, function(err, result) {
+app.get('/find', (req, res) => {
+  Customers.find({}, function (err, docs) {
     if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
+      res.send(err)
+    }
+    else {
+      res.send(docs)
     }
   });
+})
+
+
+app.post('/show', (req, res) => {
+  var cardDetails = req.body;
+  console.log(cardDetails)
+
+  omise.tokens.create(cardDetails, function (err, token) {
+    console.log('token', token.id)
+    omise.charges.create({
+      amount: 120000,
+      currency: 'thb',
+      return_uir: 'http://example.com',
+      card: token.id,
+      metadata: {
+        note: 'test card'
+      }
+    }, function (err, charge) {
+      if (err) {
+        console.log('error', err);
+        return;
+      }
+    });
+  });
+
+  return res.send('success')
 })
 
 app.get('/generateQR', (req, res) => {
   const amount = 12000;
   const mobileNumber = '0945378034';
-  const payload = generatePayload(mobileNumber, {amount})
+  const payload = generatePayload(mobileNumber, { amount })
   const option = {
     color: {
       dark: '#000',
@@ -114,5 +157,6 @@ app.get('/generateQR', (req, res) => {
     }
   })
 })
+
 
 
